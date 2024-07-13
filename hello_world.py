a3d8b97abbb350d1ef7115ec08fedf7e3a818d29
LihleAvuNgcwembe <107505046+LihleAvuNgcwembe@users.noreply.github.com>
2024-07-13 19:34:18 +0200

    Add files via upload

---
+++ hello_world.py
@@ -0,0 +1,10 @@
+#ask for user's name then store it in a string variable called user_name
+user_name = input("Please enter your name: ")
+#print data store in user_name
+print(user_name)
+#ask for user's age then store it in a string variable called user_age
+user_age = input("please enter your age: ")
+#print data stored in user_age
+print(user_age)
+#print hello world message
+print("Hello World!")
